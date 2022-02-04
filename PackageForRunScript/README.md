b67ca9fa74b84a2b097c2659cce4bdf5404e4e42
hara-94 <hikaru.h9894@gmail.com>
2022-02-04 16:32:28 +0900

    internal package

---
+++ PackageForRunScript/README.md
@@ -0,0 +1,3 @@
+# PackageForRunScript
+
+A description of this package.
---
+++ PackageForRunScript/Sources/PackageForRunScript/PackageForRunScript.swift
@@ -0,0 +1,6 @@
+public struct PackageForRunScript {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
