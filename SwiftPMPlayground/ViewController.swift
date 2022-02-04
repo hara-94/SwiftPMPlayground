69d10beeef9ccd779dd0bee066dcb4dc085ecbd5
hara-94 <hikaru.h9894@gmail.com>
2022-02-04 14:24:03 +0900

    add: samplePackage

--- SwiftPMPlayground/ViewController.swift
+++ SwiftPMPlayground/ViewController.swift
@@ -5,15 +5,25 @@
 //  Created by hikaruhara on 2022/02/04.
 //
 
+import SamplePackage
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private let button: SamplePackageButton = .init()
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+        button.render("SamplePackageButton")
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 250),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
-
-
 }
 
